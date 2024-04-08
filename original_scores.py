20a9402c93be0aabeb833ef6a039b55a7c604def
Tobias <tobiasop@gmail.com>
2024-04-08 19:05:21 +0900

    add original mean values
    Add the scores from the original articles for each question.

---
+++ original_scores.py
@@ -0,0 +1,53 @@
+# The comment is which number the question was in the Norwegian table given by the authors
+original_question_averages = {
+    "Reflect on life": 5.19,  # 24
+    "Obtain a deeper connection in life": 5.19,  # 17
+    "Opportunity for self-discovery": 4.87,  # 23
+    "Get in touch with my true self": 4.78,  # 22
+    "Find inspiration in natural surroundings": 4.98,  # 31
+    "Chance to think/solve problems": 4.38,  # 19
+    "Stimulate creativity": 3.93,  # 25
+    "Develop a sense of self-confidence": 4.52,  # 18
+    "Recreate in a primitive environment": 5.52,  # 15
+    "Observe and appreciate the ecosystem": 4.02,  # 17
+    "Explore the natural environment": 5.61,  # 2
+    "Experience nature's magic and mysticism": 3.30,  # 30
+    "Develop a oneness with nature": 5.40,  # 21
+    "Experience the scenic quality of nature": 6.20,  # 1
+    "Feel connected to a place that is important": 4.81,  # 14
+    "Being alone/solitude": 4.42,  # 7
+    "Free from observation from all other people": 3.05,  # 10
+    "Get away from daily routines": 5.42,  # 8
+    "Tranquility and peace": 5.73,  # 6
+    "Simplify daily priorities": 4.77,  # 12
+    "Physical health and exercise": 5.60,  # 11
+    "Feel a special closeness with others in my group": 3.88,  # 5
+    "Be in a small intimate group": 3.20,  # 13
+    "Feel a connection with others who value wilderness and remote places": 4.04,  # 16
+    "Having a sense of discovery": 5.00,  # 4
+    "Having an adventure": 4.35,  # 26
+    # The following was added by the SOW article
+    "Physical challenge": 5.29,  # 3
+    "Develop my religious values": 3.29,  # 9
+    "Meet local non-pilgrims along the route": 4.30,  # 20
+    "Have a story to tell": 4.41,  # 27
+    "Religious experiences": 3.41,  # 28
+    "Experience places that I have read or been told about": 4.32,  # 29
+    "The religious heritage of the trail": 3.85,  # 32
+    "Visit churches or other religious buildings": 4.47,  # 33
+    "Travel in a slow pace": 5.24,  # 34
+    "Reach the holy shrine of St. Olav": 3.93,  # 35
+    "Getting to know with foreign places and landscapes": 5.50,  # 36
+    "Travel in an environment-friendly way": 5.09,  # 37
+    "Spiritual experiences": 4.32,  # 38
+    "Buying/consuming local products (like foods or crafts)": 3.25,  # 39
+    "Visit local communities": 4.24,  # 40
+    "The medieval heritage of the trail": 4.38,  # 41
+    "Heritage sites along the trail (churches:old farm-buildings:burial mounds etc.)": 4.91,  # 42
+    "Develop my spiritual values": 4.36,  # 43
+    "Meditative experiences": 4.30,  # 44
+    "Be in the present": 5.46,  # 45
+    "Follow the rhythms of nature and landscape": 5.40,  # 46
+    "Feel free to take my time": 5.30,  # 47
+    "Discover my inner time": 4.52  # 48
+}
